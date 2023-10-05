0636b74537c52a91f9c46d1812c24ab74c7f4fb8
duy.le281 <duy.le281@hcmut.edu.vn>
2023-10-05 15:07:48 +0700

    change problem 11: two pointers

--- 11.Container_With_Most_Water.py
+++ 11.Container_With_Most_Water.py
@@ -10,13 +10,11 @@ Notice that you may not slant the container.
 
 
 Example 1:
-
-
 Input: height = [1,8,6,2,5,4,8,3,7]
 Output: 49
 Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
-Example 2:
 
+Example 2:
 Input: height = [1,1]
 Output: 1
 
@@ -63,4 +61,4 @@ class Solution(object):
                 right -= 1
         return ans
 
-print(Solution().maxArea(height = [1,1]))
+print(Solution().maxArea(height = [1,8,6,2,5,4,8,3,7]))
