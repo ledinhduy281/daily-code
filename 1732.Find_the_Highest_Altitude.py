f5297d48901177a8f36906f4345563f764740a97
duy.le281 <duy.le281@hcmut.edu.vn>
2023-10-23 17:43:12 +0700

    add problem 1732

---
+++ 1732.Find_the_Highest_Altitude.py
@@ -0,0 +1,60 @@
+'''
+
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+ 
+
+Example 1:
+
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+Example 2:
+
+Input: gain = [-4,-3,-2,-1,4,3,2]
+Output: 0
+Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+ 
+
+Constraints:
+
+n == gain.length
+1 <= n <= 100
+-100 <= gain[i] <= 100
+
+'''
+
+import collections
+import itertools
+import functools
+import math
+import string
+import random
+import bisect
+import re
+import operator
+import heapq
+import queue
+
+from icecream import ic
+from queue import PriorityQueue
+from itertools import combinations, permutations
+from functools import lru_cache
+from collections import defaultdict
+from collections import OrderedDict
+from collections import deque
+from collections import Counter
+from typing import Optional
+from typing import List
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        curGain, maxGain = 0, 0
+        for ele in gain:
+            curGain += ele
+            maxGain = max(maxGain, curGain)
+        return maxGain
+
+ic(Solution().largestAltitude(gain=[-5,1,5,0,-7]))
