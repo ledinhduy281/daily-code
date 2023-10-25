6a1f4ff2a8cf030eb048f87022d1bf2d9430f673
duy.le281 <duy.le281@hcmut.edu.vn>
2023-10-25 09:20:37 +0700

    change to problem 735

--- 735.Asteroid_Collision.py
+++ 735.Asteroid_Collision.py
@@ -1,5 +1,34 @@
 '''
-Template to run on VSCode
+We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+ 
+
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+ 
+
+Constraints:
+
+2 <= asteroids.length <= 104
+-1000 <= asteroids[i] <= 1000
+asteroids[i] != 0
 
 '''
 
@@ -26,6 +55,27 @@ from collections import Counter
 from typing import Optional
 from typing import List
 
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            # Handle the case of the asteroid moving to the right
+            if asteroid > 0:
+                stack.append(asteroid)
+            else:
+                # Handle the case of the asteroid moving to the left
+                while stack and stack[-1] > 0:
+                    # Compare the sizes of the asteroids
+                    if stack[-1] < abs(asteroid):
+                        stack.pop()
+                    elif stack[-1] == abs(asteroid):
+                        stack.pop()
+                        break
+                    else:
+                        break
+                else:
+                    stack.append(asteroid)
 
+        return stack
 
-# ic(Solution().())
+ic(Solution().asteroidCollision(asteroids=[5,10,-5]))
