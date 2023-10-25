b51901b8f0ddb25394545b1cd88e6fa8bff848c7
duy.le281 <duy.le281@hcmut.edu.vn>
2023-10-25 08:57:54 +0700

    add problem 2390

---
+++ 2390.Removing_Stars_From_a_String.py
@@ -0,0 +1,64 @@
+'''
+You are given a string s, which contains stars *.
+
+In one operation, you can:
+
+Choose a star in s.
+Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
+
+Note:
+
+The input will be generated such that the operation is always possible.
+It can be shown that the resulting string will always be unique.
+
+Example 1:
+
+Input: s = "leet**cod*e"
+Output: "lecoe"
+Explanation: Performing the removals from left to right:
+- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+There are no more stars, so we return "lecoe".
+Example 2:
+
+Input: s = "erase*****"
+Output: ""
+Explanation: The entire string is removed, so we return an empty string.
+
+'''
+
+import collections
+import itertools
+import functools
+import math
+import string
+import random
+import bisect
+import re
+import operator
+import heapq
+import queue
+
+from icecream import ic
+from queue import PriorityQueue
+from itertools import combinations, permutations
+from functools import lru_cache
+from collections import defaultdict
+from collections import OrderedDict
+from collections import deque
+from collections import Counter
+from typing import Optional
+from typing import List
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+        ans = []
+        for i in s:
+            if i == '*':
+                ans.pop()
+            else:
+                ans.append(i)
+        return ''.join(ans)
+ic(Solution().removeStars(s="leet**cod*e"))
