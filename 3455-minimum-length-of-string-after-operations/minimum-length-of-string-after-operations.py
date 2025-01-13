5f330d292152d3fb65bb9714e04ee90bfff81f49
KTANE <49335381+ledinhduy281@users.noreply.github.com>
2025-01-13 22:35:52 +0700

    Time: 65 ms (88.25%) | Memory: 18.7 MB (52.38%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,3 +1,13 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        return sum(2 - c % 2 for c in Counter(s).values())
+        # return sum(2 - c % 2 for c in Counter(s).values())
+
+        s_set = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+        ans = 0
+        for ch in s_set:
+            count = s.count(ch)
+            if count & 1:
+                ans += 1
+            elif count != 0:
+                ans += 2
+        return ans
