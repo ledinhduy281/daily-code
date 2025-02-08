4a50509cd4966adef6333b5cf88dd9574e893d8d
KTANE <49335381+ledinhduy281@users.noreply.github.com>
2025-02-08 10:49:47 +0700

    Time: 3 ms (93.81%) | Memory: 18 MB (87.63%) - LeetSync

--- 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.py
+++ 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.py
@@ -1,29 +1,43 @@
 from math import comb
 class Solution:
     def countOfPairs(self, nums: List[int]) -> int:
-        MOD = 10**9 + 7
-        a, b = 0, nums[0]
-        arr1 = [a]
-        arr2 = [b]
-        for i in nums[1:]:
-            # min a possible
-            a = max(a, i - b)
-            b = i - a
-            arr1.append(a)
-            arr2.append(b)
-        # probablity question
-        # bars and stars
-        # we have to convert 5, 5, 5, 5 to 0, 0, 0, 0 in the end
-        # xi can be {0, 1, 2, 3, 4, 5}
-        # to find the number of times xi appears is the problem
-        # x0 + x1 + x2 + x3 + x4 + x5 = 4
+        # MOD = 10**9 + 7
+        # a, b = 0, nums[0]
+        # arr1 = [a]
+        # arr2 = [b]
+        # for i in nums[1:]:
+        #     # min a possible
+        #     a = max(a, i - b)
+        #     b = i - a
+        #     arr1.append(a)
+        #     arr2.append(b)
+        # # probablity question
+        # # bars and stars
+        # # we have to convert 5, 5, 5, 5 to 0, 0, 0, 0 in the end
+        # # xi can be {0, 1, 2, 3, 4, 5}
+        # # to find the number of times xi appears is the problem
+        # # x0 + x1 + x2 + x3 + x4 + x5 = 4
         
-        # number of bars = 5
-        # number of stars = 4
-        # 9 choose 4 
-        arr_2_min = min(arr2)
-        if arr_2_min < 0:
-            return 0
-        res = comb(arr_2_min + len(arr2), len(arr2))
+        # # number of bars = 5
+        # # number of stars = 4
+        # # 9 choose 4 
+        # arr_2_min = min(arr2)
+        # if arr_2_min < 0:
+        #     return 0
+        # res = comb(arr_2_min + len(arr2), len(arr2))
         
-        return res % MOD
+        # return res % MOD
+
+        n, mn = len(nums), nums.pop(0)
+        num1, num2 = 0, mn
+    
+        for num in nums:
+            if num < num1: return 0
+
+            if num > num1 + num2: 
+                num1 = num - num2  
+
+            num2 = num - num1
+
+            if num2 < mn: mn = num2
+        return comb(n + mn, mn) % 1_000_000_007
