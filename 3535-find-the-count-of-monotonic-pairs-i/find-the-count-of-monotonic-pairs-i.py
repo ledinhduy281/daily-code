fd46d6c73934816c0caff3828ec4f48ffd5b1bc7
KTANE <49335381+ledinhduy281@users.noreply.github.com>
2025-02-08 10:51:44 +0700

    Time: 0 ms (100.00%) | Memory: 18.3 MB (70.10%) - LeetSync

--- 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.py
+++ 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.py
@@ -28,16 +28,34 @@ class Solution:
         
         # return res % MOD
 
-        n, mn = len(nums), nums.pop(0)
-        num1, num2 = 0, mn
+        # n, mn = len(nums), nums.pop(0)
+        # num1, num2 = 0, mn
     
+        # for num in nums:
+        #     if num < num1: return 0
+
+        #     if num > num1 + num2: 
+        #         num1 = num - num2  
+
+        #     num2 = num - num1
+
+        #     if num2 < mn: mn = num2
+        # return comb(n + mn, mn) % 1_000_000_007
+
+        mi, mx = 0, nums[0]
         for num in nums:
-            if num < num1: return 0
-
-            if num > num1 + num2: 
-                num1 = num - num2  
-
-            num2 = num - num1
-
-            if num2 < mn: mn = num2
-        return comb(n + mn, mn) % 1_000_000_007
+            if num - mi < 0:
+                return 0
+            
+            if num >= mi + mx:
+                mi = num - mx # Really just = mi + num - (mi + mx)
+            
+            mx = num - mi
+        """
+        Every digit within either array can vary from its max to min by a maximum of (mx + 1).
+        E.g. for decreasing array, the last digit can be [0, mx], length = mx + 1
+        Therefore, every pair is just each of the (mx+1) increments taking place at different
+        positions within the array. Equivalent ways to split array into (mx+1) == putting len(nums) into (mx+1) buckets.
+        (len(nums) + (mx+1) - 1) choose len(nums)
+        """
+        return math.comb(len(nums)+mx, mx) % (10**9 + 7)
