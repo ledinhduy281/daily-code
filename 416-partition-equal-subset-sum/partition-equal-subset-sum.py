2a955cdb423acb64053ddce6382c7a6d128e423f
KTANE <49335381+ledinhduy281@users.noreply.github.com>
2025-04-07 08:55:59 +0700

    Time: 0 ms (100.00%) | Memory: 18.2 MB (72.63%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,26 +1,38 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
 
-        sum_nums = sum(nums)
+        # sum_nums = sum(nums)
 
-        if sum_nums % 2 != 0:
+        # if sum_nums % 2 != 0:
+        #     return False
+
+        # half_sum_nums = sum(nums) // 2
+        # sum_element = 0
+
+        # # for i in range(len(nums)):
+        # #     sum_element = nums[i]
+        # #     for j in range(i+1, len(nums)):
+        # #         sum_element += nums[j]
+        # #         if sum_element == half_sum_nums:
+        # #             return True
+        # #         elif sum_element > half_sum_nums:
+        # #             break
+        # # return False
+        # dp = [False] * (half_sum_nums + 1)
+        # dp[0] = True
+        # for num in nums:
+        #     for current_sum in range(half_sum_nums, num - 1, -1):
+        #         dp[current_sum] = dp[current_sum] or dp[current_sum - num]
+        # return dp[half_sum_nums]
+
+        total = sum(nums)
+        if total % 2 != 0:
             return False
 
-        half_sum_nums = sum(nums) // 2
-        sum_element = 0
+        target = total // 2
+        dp = 1 << 0
 
-        # for i in range(len(nums)):
-        #     sum_element = nums[i]
-        #     for j in range(i+1, len(nums)):
-        #         sum_element += nums[j]
-        #         if sum_element == half_sum_nums:
-        #             return True
-        #         elif sum_element > half_sum_nums:
-        #             break
-        # return False
-        dp = [False] * (half_sum_nums + 1)
-        dp[0] = True
         for num in nums:
-            for current_sum in range(half_sum_nums, num - 1, -1):
-                dp[current_sum] = dp[current_sum] or dp[current_sum - num]
-        return dp[half_sum_nums]
+            dp |= dp << num
+
+        return (dp & (1 << target)) != 0
