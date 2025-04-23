55023885ca242d7c42f4aecfd9d52fdc1030435d
KTANE <49335381+ledinhduy281@users.noreply.github.com>
2025-04-23 09:30:54 +0700

    Time: 7 ms (99.50%) | Memory: 17.8 MB (53.00%) - LeetSync

--- 1500-count-largest-group/count-largest-group.py
+++ 1500-count-largest-group/count-largest-group.py
@@ -1,9 +1,19 @@
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        hashMap = collections.Counter()
-        for i in range(1, n + 1):
-            key = sum([int(x) for x in str(i)])
-            hashMap[key] += 1
-        maxValue = max(hashMap.values())
-        count = sum(1 for v in hashMap.values() if v == maxValue)
-        return count
+        # hashMap = collections.Counter()
+        # for i in range(1, n + 1):
+        #     key = sum([int(x) for x in str(i)])
+        #     hashMap[key] += 1
+        # maxValue = max(hashMap.values())
+        # count = sum(1 for v in hashMap.values() if v == maxValue)
+        # return count
+
+        categorii=[0]*37 #ignor 0. am 36 de categorii
+        tablouSumaCifrelor=[0]*(n+1)
+        for i in range(1,n+1):
+            sc=i%10+tablouSumaCifrelor[i//10]
+            tablouSumaCifrelor[i]=sc
+            categorii[sc] += 1
+        valMax=max(categorii)
+        nrGrupuri=categorii.count(valMax)
+        return nrGrupuri
